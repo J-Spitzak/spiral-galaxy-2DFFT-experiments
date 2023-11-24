1683646e79c6c448e1ce4ae8ae073fafb4d06140
Jason Spitzak <jason.spitzak@gmail.com>
2023-11-24 15:02:25 -0500

    added image cutout program

---
+++ FITS_files/image_cutout.py
@@ -0,0 +1,28 @@
+# Download an example FITS file, create a 2D cutout, and save it to a
+# new FITS file, including the updated cutout WCS.
+from astropy.io import fits
+from astropy.nddata import Cutout2D
+from astropy.utils.data import download_file
+from astropy.wcs import WCS
+
+# Download the image
+filename = 'frame-g-002141-2-0115.fits'
+position = (853,791)
+size = 200
+
+# Load the image and the WCS
+hdu = fits.open(filename)[0]
+wcs = WCS(hdu.header)
+
+# Make the cutout, including the WCS
+cutout = Cutout2D(hdu.data, position=position, size=size, wcs=wcs)
+
+ # Put the cutout image in the FITS HDU
+hdu.data = cutout.data
+
+# Update the FITS header with the cutout WCS
+hdu.header.update(cutout.wcs.to_header())
+
+# Write the cutout to a new FITS file
+cutout_filename = 'example_cutout.fits'
+hdu.writeto(cutout_filename, overwrite=True)
